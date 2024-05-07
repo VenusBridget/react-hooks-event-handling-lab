dcc893861d8d4390ebd593e4c4a852f6e47cfb32
VenusBridget <venusbri27@gmail.com>
2024-05-07 14:29:31 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  return (
+        <button on Blur = {() => {
+            console.log('Hey! Eyes on me!')
+        
+        }}
+        onFocus = {() => {
+            console.log('Good!')
+        }}>Eyes On Me</button>
+  )
+  
+}
+
+export default EyesOnMe
